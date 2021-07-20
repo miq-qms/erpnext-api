04d62d156c05e7e860cc4e5054b2e7292a586abf
Konstantin Tümmler <konstantin.tuemmler@miq.com.de>
2021-07-20 10:39:09 +0200

    api exception

---
+++ src/Exception/ApiException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Miq\ErpnextApi\Exception;
+
+use Exception;
+
+/**
+ * Class ApiException
+ * @package Miq\ErpnextApi\Exception
+ *
+ * Code: HTTP Response Code
+ * Message: Message from API Endpoint (e.g. invalid Data)
+ */
+class ApiException extends Exception {
+
+}
