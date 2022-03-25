22bd5501acdb807a68670e936bedbe06918bf05b
MiQ42 <arian.mingo@miq.com.de>
2022-03-25 14:20:58 +0100

    DocType Operation

--- src/Entity/Operation.php
+++ src/Entity/Operation.php
@@ -49,4 +49,84 @@ class Operation
      * @ORM\Column(name="description", type="text", length=65535, nullable=true)
      */
     private ?string $description;
+
+    /**
+     * @return string|null
+     */
+    public function getParent(): ?string
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param string|null $parent
+     */
+    public function setParent(?string $parent): void
+    {
+        $this->parent = $parent;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getParentfield(): ?string
+    {
+        return $this->parentfield;
+    }
+
+    /**
+     * @param string|null $parentfield
+     */
+    public function setParentfield(?string $parentfield): void
+    {
+        $this->parentfield = $parentfield;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getParenttype(): ?string
+    {
+        return $this->parenttype;
+    }
+
+    /**
+     * @param string|null $parenttype
+     */
+    public function setParenttype(?string $parenttype): void
+    {
+        $this->parenttype = $parenttype;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getWorkstation(): ?string
+    {
+        return $this->workstation;
+    }
+
+    /**
+     * @param string|null $workstation
+     */
+    public function setWorkstation(?string $workstation): void
+    {
+        $this->workstation = $workstation;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
 }
