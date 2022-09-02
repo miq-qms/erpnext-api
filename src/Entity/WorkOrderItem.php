70049d8af8e0b8b557da373bff453c4174ec9d5d
Konstantin Tümmler <konstantin.tuemmler@miq.com.de>
2022-09-02 10:09:11 +0200

    WorkOrderItem

--- src/Entity/WorkOrderItem.php
+++ src/Entity/WorkOrderItem.php
@@ -2,7 +2,6 @@
 
 namespace Miq\ErpnextApi\Entity;
 
-use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Miq\ErpnextApi\Traits\DefaultTrait;
 
@@ -14,126 +13,126 @@ use Miq\ErpnextApi\Traits\DefaultTrait;
  */
 class WorkOrderItem
 {
-  use DefaultTrait;
+    use DefaultTrait;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="operation", type="string", length=140, nullable=true)
      */
-    private $operation;
+    private ?string $operation;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="item_code", type="string", length=140, nullable=true)
      */
-    private $itemCode;
+    private ?string $itemCode;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="source_warehouse", type="string", length=140, nullable=true)
      */
-    private $sourceWarehouse;
+    private ?string $sourceWarehouse;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="item_name", type="string", length=140, nullable=true)
      */
-    private $itemName;
+    private ?string $itemName;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="description", type="text", length=65535, nullable=true)
      */
-    private $description;
+    private ?string $description;
 
     /**
      * @var int
      *
      * @ORM\Column(name="allow_alternative_item", type="integer", nullable=false)
      */
-    private $allowAlternativeItem = '0';
+    private int $allowAlternativeItem = 0;
 
     /**
      * @var int
      *
      * @ORM\Column(name="include_item_in_manufacturing", type="integer", nullable=false)
      */
-    private $includeItemInManufacturing = '0';
+    private int $includeItemInManufacturing = 0;
 
     /**
-     * @var string
+     * @var float
      *
-     * @ORM\Column(name="required_qty", type="decimal", precision=21, scale=9, nullable=false, options={"default"="0.000000000"})
+     * @ORM\Column(name="required_qty", type="decimal", precision=21, scale=9, nullable=false, options={"default"=0.000000000})
      */
-    private $requiredQty = '0.000000000';
+    private float $requiredQty = 0.000000000;
 
     /**
-     * @var string
+     * @var float
      *
-     * @ORM\Column(name="rate", type="decimal", precision=21, scale=9, nullable=false, options={"default"="0.000000000"})
+     * @ORM\Column(name="rate", type="decimal", precision=21, scale=9, nullable=false, options={"default"=0.000000000})
      */
-    private $rate = '0.000000000';
+    private float $rate = 0.000000000;
 
     /**
-     * @var string
+     * @var float
      *
-     * @ORM\Column(name="amount", type="decimal", precision=21, scale=9, nullable=false, options={"default"="0.000000000"})
+     * @ORM\Column(name="amount", type="decimal", precision=21, scale=9, nullable=false, options={"default"=0.000000000})
      */
-    private $amount = '0.000000000';
+    private float $amount = 0.000000000;
 
     /**
-     * @var string
+     * @var float
      *
-     * @ORM\Column(name="transferred_qty", type="decimal", precision=21, scale=9, nullable=false, options={"default"="0.000000000"})
+     * @ORM\Column(name="transferred_qty", type="decimal", precision=21, scale=9, nullable=false, options={"default"=0.000000000})
      */
-    private $transferredQty = '0.000000000';
+    private float $transferredQty = 0.000000000;
 
     /**
-     * @var string
+     * @var float
      *
-     * @ORM\Column(name="consumed_qty", type="decimal", precision=21, scale=9, nullable=false, options={"default"="0.000000000"})
+     * @ORM\Column(name="consumed_qty", type="decimal", precision=21, scale=9, nullable=false, options={"default"=0.000000000})
      */
-    private $consumedQty = '0.000000000';
+    private float $consumedQty = 0.000000000;
 
     /**
-     * @var string
+     * @var float
      *
-     * @ORM\Column(name="available_qty_at_source_warehouse", type="decimal", precision=21, scale=9, nullable=false, options={"default"="0.000000000"})
+     * @ORM\Column(name="available_qty_at_source_warehouse", type="decimal", precision=21, scale=9, nullable=false, options={"default"=0.000000000})
      */
-    private $availableQtyAtSourceWarehouse = '0.000000000';
+    private float $availableQtyAtSourceWarehouse = 0.000000000;
 
     /**
-     * @var string
+     * @var float
      *
-     * @ORM\Column(name="available_qty_at_wip_warehouse", type="decimal", precision=21, scale=9, nullable=false, options={"default"="0.000000000"})
+     * @ORM\Column(name="available_qty_at_wip_warehouse", type="decimal", precision=21, scale=9, nullable=false, options={"default"=0.000000000})
      */
-    private $availableQtyAtWipWarehouse = '0.000000000';
+    private float $availableQtyAtWipWarehouse = 0.000000000;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="parent", type="string", length=140, nullable=true)
      */
-    private $parent;
+    private ?string $parent;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="parentfield", type="string", length=140, nullable=true)
      */
-    private $parentfield;
+    private ?string $parentfield;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="parenttype", type="string", length=140, nullable=true)
      */
-    private $parenttype;
+    private ?string $parenttype;
 
 
 }
